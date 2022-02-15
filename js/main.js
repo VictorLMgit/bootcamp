bc96e50ff1703a6c565d6686f9e584e046cc2f93
VictorLMgit <victorlimacar@gmail.com>
2022-02-15 19:23:10 -0300

    Implement Newsletter

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function changeColor(){
-const colors = ["red", "yellow", "blue","pink","black","white","browm","aqua","blueviolet","firebrick","cornflowerblue","coral","green"];
+const colors = ["yellow", "blue","pink","black","white","browm","aqua","blueviolet","firebrick","cornflowerblue","coral","green", "purple"];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     $('span').css("color",randomColor);
 }
