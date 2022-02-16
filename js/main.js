ddb91b7b97be045406aa81e09e7341be4581e8fa
VictorLMgit <victorlimacar@gmail.com>
2022-02-16 11:40:00 -0300

    create a contact and map area

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
+
 $('.showcase-text').hide(0).show(1000)
 function changeColor(){
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     const randomColor = "rgb("+r+","+g+","+b+")";
-    $('span').css("color",randomColor);
+    $('.change').css("color",randomColor);
 }
